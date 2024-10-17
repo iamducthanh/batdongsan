482f9215ecd89ca417ae8270185fd8ad9fd209d0
iamducthanh <65590543+iamducthanh@users.noreply.github.com>
2024-10-17 22:27:00 +0700

    init project

---
+++ src/test/java/org/batdongsan/batdongsan/BatdongsanApplicationTests.java
@@ -0,0 +1,13 @@
+package org.batdongsan.batdongsan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatdongsanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
